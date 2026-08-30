b5851ff9496e47c4cce8ab462b2c523ca9be78d2
Ranilo John Delos Angeles <ranilojohn444@gmail.com>
2026-08-30 15:02:17 +0800

    Update Hero profile image to DrDOOM.png

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -49,7 +49,7 @@ const Hero = () => {
                     <div className={styles.imageContent}>
                         <div className={styles.imageWrapper}>
                             <Image
-                                src={getImagePath('/images/ranilojohn.jpg')}
+                                src={getImagePath('/images/DrDOOM.png')}
                                 alt="Ranilo John Delos Angeles"
                                 width={240}
                                 height={240}
